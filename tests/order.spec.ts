223e9442790be0ffaa01e67a19e5a4391aed504b
nocotymowisz <kamil.lukasik.91@gmail.com>
2024-08-11 21:52:38 +0200

    aaa implementation

--- tests/order.spec.ts
+++ tests/order.spec.ts
@@ -2,19 +2,30 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Orders', () => {
   test('succesful order complete', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/');
-    await page.locator('#user-name').fill('standard_user');
-    await page.locator('#password').fill('secret_sauce');
+    // Arrange
+    const url = 'https://www.saucedemo.com/';
+    const userName = 'standard_user';
+    const userPassword = 'secret_sauce';
+
+    const firstName = 'Test';
+    const lastName = 'Testowy';
+    const postCode = '12-123';
+
+    // Act
+    await page.goto(url);
+    await page.locator('#user-name').fill(userName);
+    await page.locator('#password').fill(userPassword);
     await page.locator('#login-button').click();
     await page.locator('#add-to-cart-sauce-labs-backpack').click();
     await page.locator('#shopping_cart_container').click();
     await page.locator('#checkout').click();
-    await page.locator('#first-name').fill('Test');
-    await page.locator('#last-name').fill('Testowy');
-    await page.locator('#postal-code').fill('12-123');
+    await page.locator('#first-name').fill(firstName);
+    await page.locator('#last-name').fill(lastName);
+    await page.locator('#postal-code').fill(postCode);
     await page.locator('#continue').click();
     await page.locator('#finish').click();
 
+    // Assert
     await expect(page.locator('[data-test="complete-header"]')).toHaveText('Thank you for your order!');
   });
 
